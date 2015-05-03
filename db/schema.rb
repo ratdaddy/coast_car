9253214b3374284503e5a06d33d955406b7d5fd3
Brian VanLoo <brian.vanloo@gmail.com>
2015-05-03 07:50:34 -0700

    Add email to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503030452) do
+ActiveRecord::Schema.define(version: 20150503144624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150503030452) do
     t.integer  "rider_preference_food"
     t.integer  "rider_preference_children"
     t.integer  "rider_preference_conversation"
+    t.string   "email"
   end
 
 end
